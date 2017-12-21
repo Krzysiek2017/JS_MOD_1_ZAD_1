254262334d32be0bbb0a029e601bb6d58eaf5c63
Logan <musashimiyamoto@interia.eu>
2017-12-21 15:47:49 +0100

    wciecia

--- zad1.js
+++ zad1.js
@@ -17,26 +17,25 @@ process.stdin.on('readable', function() {
     }
 });
 */
-
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
         switch (instruction) {
-        case ('/exit'):
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-            break;
-        case ('/ver'):
-            console.log(process.env);
-            break;
-        case ('/działa?'):
-            console.log('działa!');
-            break;
-        default:
-            process.stderr.write('Wrong instruction!\n');
-            break;
-     }
+            case ('/exit'):
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case ('/ver'):
+                console.log(process.env);
+                break;
+            case ('/działa?'):
+                console.log('działa!');
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+                break;
+        }
     }
 });
